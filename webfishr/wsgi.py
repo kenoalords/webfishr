2a4b42985368652f3b85a0a09fbbb18a50ce1728
kenoalords <kenoalords@gmail.com>
2021-02-17 17:17:39 +0100

    ui upgrade

--- webfishr/wsgi.py
+++ webfishr/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webfishr.settings')
 
 application = get_wsgi_application()
-
-import djcelery
-djcelery.setup_loader()
